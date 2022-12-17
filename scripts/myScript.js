838e03026fa9ecc83e195c2f332727c5381ee79b
Adnaan Rahman <X00167798@mytudublin.ie>
2022-12-17 16:18:17 +0000

    remove not working hover effect

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,14 +1,3 @@
-// Navbar Hover Effect
-item.onmouseenter = function(){
-    this.classList.add('hovered');
-    }
-    
-    item.onmouseleave = function(){
-    setTimeout(function(){
-    this.classList.remove('hovered');
-    }.bind(this),1000)
-    }
-
 // Contact Me Popup Window
 function buttonSubmit() {
     alert("Form has been submitted");
